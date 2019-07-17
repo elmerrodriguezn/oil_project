366ffc8b3abe93c185b3fc6ddf826d6ccc0fdbee
Elmer Rodriguez <elmer.rodriguezn@gmail.com>
2019-07-17 10:00:37 -0700

    fixed addSearchBar on scroll

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
-const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
+var getYear;
+(getYear = function () {
+    const date = new Date();
+    document.querySelector('.year').innerHTML = date.getFullYear();
+})();
 
-function addSearchBar() {
+var addSearchBar;
+(addSearchBar = function () {
     if (window.location.pathname === '/') {
         if (window.pageYOffset > 700) {
             document.getElementById("search-bar").style.display = "block";
-        } else {
+        } else if (window.pageYOffset < 100) {
             document.getElementById("search-bar").style.display = "none";
         }
     } else {
         document.getElementById("search-bar").style.display = "block";
     }
-}
-addSearchBar();
+})();
